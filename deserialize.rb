93ebcc61d9d1840ce1821d5dcc9fda8b97227414
Colkimi Mibey <141415279+Colkimi@users.noreply.github.com>
2025-04-16 19:57:00 +0300

    Create deserialize.rb

---
+++ deserialize.rb
@@ -0,0 +1,10 @@
+# Only use in a controlled environment!
+payload = Marshal.dump({ :command => "id" })
+
+# Simulate unsafe deserialization (if system uses `Marshal.load(user_input)`)
+begin
+  data = Marshal.load(payload)
+  puts "Deserialized: #{data.inspect}"
+rescue => e
+  puts "Error: #{e.message}"
+end
