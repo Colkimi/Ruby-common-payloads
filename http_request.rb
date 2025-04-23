1d6c16df6e715e7a8a79b3348a2789f4b29eb6c2
Colkimi Mibey <141415279+Colkimi@users.noreply.github.com>
2025-04-23 20:09:15 +0300

    Update http_request.rb

--- http_request.rb
+++ http_request.rb
@@ -1,6 +1,6 @@
 require 'net/http'
 
-# Test for SSRF (Server-Side Request Forgery)
+# Test for SSRF (Server-Side Request Forgery
 url = URI.parse("http://internal-server.local/admin")
 res = Net::HTTP.get_response(url)
 puts res.body if res.is_a?(Net::HTTPSuccess)
